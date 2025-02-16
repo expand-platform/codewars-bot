6f630542aa8ddbfe242838a16f07c33ef887ede0
Danya <ovrashkod@gmail.com>
2025-02-16 12:08:36 +0200

    new admin features

---
+++ src/messages/admin_messages.py
@@ -0,0 +1,3 @@
+ADMIN_MESSAGES = {
+    "admin_commands_list": "Only admin can see this message!\n\nHere is a list of admin commands:\n/load_tasks\n/show_analytics\n/user_add_property\n/user_delete_property"
+}
